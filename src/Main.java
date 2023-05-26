1b8e8292c21d7fffeee0a93cb355826189506ddc
CheaTXT <serey.vichea9999@gmail.com>
2023-05-26 16:23:35 +0700

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application implements App, App {
+public class Main extends Application {
+    public Main() {
+    }
+
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
-    @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("login.fxml"));
         stage.setTitle("Welcome to Library App");
-        stage.setScene(new Scene(root, 500, 500));
+        stage.setScene(new Scene(root, 500.0, 500.0));
         stage.show();
     }
 }
