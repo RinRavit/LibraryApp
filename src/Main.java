55a5f0ba936cde1b7d38ad6c696d813cb9cf5a5d
CheaTXT <serey.vichea9999@gmail.com>
2023-05-26 08:57:06 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class main extends Application {
+public class Main extends Application implements App, App {
     public static void main(String[] args) throws Exception {
         launch(args);
     }
