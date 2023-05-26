28dab13ddfd58c1b0e59e1e1967071b36fd30e20
CheaTXT <serey.vichea9999@gmail.com>
2023-05-26 16:29:06 +0700

    Library

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("login.fxml"));
         stage.setTitle("Welcome to Library App");
-        stage.setScene(new Scene(root, 500.0, 500.0));
+        stage.setScene(new Scene(root, 500, 500));
         stage.show();
     }
 }
